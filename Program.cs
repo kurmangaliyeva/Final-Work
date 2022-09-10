c56e3840fb15ca2a5016be53c6a42a092231aa34
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-09-11 00:58:01 +0600

    Array sizing loop

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Final_Work{
+    class Program{
+        static void Main(string[] args){
+
+
+            String? yesNo = "";
+            while (yesNo.ToLower() != "yes" && yesNo.ToLower() != "no"){
+            Console.Write("Желаете вводить строки вручную? (yes/no): ");
+            yesNo = Console.ReadLine();
+            }
+
+
+
+
+
+        }
+    }
+}
