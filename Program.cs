17fd1712329192777c18381794e0932552be9011
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-09-11 01:07:25 +0600

    next step: entering arraay

--- Program.cs
+++ Program.cs
@@ -7,8 +7,23 @@ namespace Final_Work{
 
             String? yesNo = "";
             while (yesNo.ToLower() != "yes" && yesNo.ToLower() != "no"){
-            Console.Write("Желаете вводить строки вручную? (yes/no): ");
+            Console.Write("Желаете вводить строки массива вручную? (yes/no): ");
             yesNo = Console.ReadLine();
+            }
+
+            string[] arrayOfStrings = new string[]  {};
+
+            if (yesNo.ToLower() == "yes") {
+            int n = InputNumbers("Введите количество элементов массива: ");
+            arrayOfStrings = new string[n];
+            for (int i = 0; i < arrayOfStrings.Length; i++){
+                Console.Write($" Введите {i+1} строку: ");
+                arrayOfStrings[i] = Console.ReadLine();
+                }
+            }
+            
+            else {
+            arrayOfStrings = new string[]  {"hello", "2", "world", ":-)"};
             }
 
 
