50b6024cdc68c5287f134cfa13d55ba277a1ba3a
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-09-11 13:47:56 +0600

    next step: printing new array

--- Program.cs
+++ Program.cs
@@ -53,7 +53,23 @@ namespace Final_Work{
             return result;
             }
 
+            string PrintArray(string[] array){ 
+            string result = string.Empty;
+            result = "[ ";
+            for (int i = 0; i < array.Length; i++){
+                result += $"{array[i],1}";
+                if (i < array.Length - 1) result += ", ";
+                }
 
+            result += " ]";
+            return result;
+            }
+
+            int InputNumbers(string input){
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+            }
 
         }
     }
